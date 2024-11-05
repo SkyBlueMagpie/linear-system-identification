ca5c97004d81258664dac136d5d99736adaa25db
SkyBlueMagpie <124182034+SkyBlueMagpie@users.noreply.github.com>
2024-11-05 18:41:47 +0800

    Update RLSM.m

--- RLSM.m
+++ RLSM.m
@@ -1,14 +1,14 @@
-function [theta2,P2] = RLSM(theta1,P1,phi,zN)
-%最小二乘递推算法
-%phi是由[z(N),z(N-1),...,z(N+1-n),u(N+1),...,u(N+1-n)]组成的列向量
-%zN是当前输出
-%其余参数均为列向量
-gamma2=1/(1+phi'*P1*phi);
-K2=gamma2*P1*phi;
-P2=P1-K2*phi'*P1;
-err=zN-phi'*theta1;
-%P2=P1-(P1*phi*phi'*P1)/(1+phi'*P1*phi);
+function [theta2,P2] = RLSM(theta1,P1,phi)
+%Recursive least squares algorithm
+%All vectors are column vectors
+%phi is [z(N+1),z(N-1),...,z(N+1-n),u(N+1),...,u(N+1-s)],
+%You may need to fill in the 0 for the start of the input/output signal(phi) yourself.
+%Matrix P is used for recursion, you can assign an initial value of 1e6*eye at the beginning.
+%theta is the parameter to be identified, described by the readme file.
+gamma2=1/(1+phi(2:end)'*P1*phi(2:end));
+K2=gamma2*P1*phi(2:end);
+P2=P1-K2*phi(2:end)'*P1;
+err=phi(1)-phi(2:end)'*theta1;
 theta2=theta1+K2*err;
-%theta2=theta1+P1*phi/(1+phi'*P1*phi)*err;
 end
 
