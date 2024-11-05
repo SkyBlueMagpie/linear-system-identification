742c916bbcc4efd6cb824ac4ed4881734b3c1eb8
SkyBlueMagpie <124182034+SkyBlueMagpie@users.noreply.github.com>
2024-11-05 18:49:51 +0800

    Update RELS.m

--- RELS.m
+++ RELS.m
@@ -1,12 +1,12 @@
-function [theta2,P2,omega] = RELS(theta1,P1,phi,zN)
-%递推增广最小二乘
-%y(k)+a1y(k-1)+...+any(k-n)=b0u(k)+b1u(k-1)+...+bnu(k-n)+w(k)+c1w(k-1)+...+cnw(k-n)
+function [theta2,P2,omega] = RELS(theta1,P1,phi)
+%Recursive extended least squares method
+%y(k)+a1y(k-1)+...+any(k-n)=b0u(k)+b1u(k-1)+...+bsu(k-n)+w(k)+c1w(k-1)+...+cmw(k-n)
 %theta'=[a1,...,an,b0,...,bn,...,c1,...,cn]
-%phi'=[-z(k-1),...,-z(k-n),u(k),...,u(k-n),w(k-1),...,w(k-n)]
-%zN当前输出
-K2=P1*phi/(1+phi'*P1*phi);
-P2=P1-K2*phi'*P1;
-theta2=theta1+K2*(zN-phi'*theta1);
-omega=zN-phi'*theta2;
+%phi'=[z(k),z(k-1),...,z(k-n),u(k),...,u(k-n),w(k-1),...,w(k-n)]
+%You should add omega to vector phi's w(k).
+K2=P1*phi(2:end)/(1+phi(2:end)'*P1*phi(2:end));
+P2=P1-K2*phi(2:end)'*P1;
+theta2=theta1+K2*(zN-phi(2:end)'*theta1);
+omega=phi(1)-phi(2:end)'*theta2;
 end
 
