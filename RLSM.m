fde6fc27f6ede82e85fbe49ffd62f36bde05a125
SkyBlueMagpie <124182034+SkyBlueMagpie@users.noreply.github.com>
2024-11-05 18:52:09 +0800

    Update RLSM.m

--- RLSM.m
+++ RLSM.m
@@ -1,6 +1,7 @@
 function [theta2,P2] = RLSM(theta1,P1,phi)
 %Recursive least squares algorithm
 %All vectors are column vectors
+%y(k)+a1y(k-1)+...+any(k-n)=b0u(k)+b1u(k-1)+...+bsu(k-n)+w(k)
 %phi is [z(N+1),z(N-1),...,z(N+1-n),u(N+1),...,u(N+1-s)],
 %You may need to fill in the 0 for the start of the input/output signal(phi) yourself.
 %Matrix P is used for recursion, you can assign an initial value of 1e6*eye at the beginning.
