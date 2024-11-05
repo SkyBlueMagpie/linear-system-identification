49bc19cb232b2c10a527d25e95df09681174e078
SkyBlueMagpie <124182034+SkyBlueMagpie@users.noreply.github.com>
2024-11-05 18:50:48 +0800

    Update RELS.m

--- RELS.m
+++ RELS.m
@@ -2,8 +2,10 @@ function [theta2,P2,omega] = RELS(theta1,P1,phi)
 %Recursive extended least squares method
 %y(k)+a1y(k-1)+...+any(k-n)=b0u(k)+b1u(k-1)+...+bsu(k-n)+w(k)+c1w(k-1)+...+cmw(k-n)
 %theta'=[a1,...,an,b0,...,bn,...,c1,...,cn]
-%phi'=[z(k),z(k-1),...,z(k-n),u(k),...,u(k-n),w(k-1),...,w(k-n)]
+%phi'=[z(k),z(k-1),...,z(k-n),u(k),...,u(k-s),w(k-1),...,w(k-m)]
 %You should add omega to vector phi's w(k).
+%You may need to fill in the 0 for the start of the input/output signal(phi) yourself.
+%Matrix P is used for recursion, you can assign an initial value of 1e6*eye at the beginning.
 K2=P1*phi(2:end)/(1+phi(2:end)'*P1*phi(2:end));
 P2=P1-K2*phi(2:end)'*P1;
 theta2=theta1+K2*(zN-phi(2:end)'*theta1);
